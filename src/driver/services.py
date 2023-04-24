a21c0504294fb18532e744871091b774154dfc68
Uzair Popalzai <up18707@essex.ac.uk>
2023-04-24 17:02:49 +0100

    reverted to  geckodriver

--- src/driver/services.py
+++ src/driver/services.py
@@ -1,71 +1,85 @@
 import asyncio
+import os
 from typing import Optional
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.webdriver import WebDriver
 from fastapi import HTTPException
 from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from src.driver.config import sa_settings
 
-BROWSER_OPTIONS = Options()
-BROWSER_OPTIONS.binary_location = sa_settings.google_chrome_bin
-BROWSER_OPTIONS.add_argument("--headless")
-BROWSER_OPTIONS.add_argument("--no-sandbox")
-BROWSER_OPTIONS.add_argument("--disable-dev-shm-usage")
-BROWSER_OPTIONS.add_argument("--disable-gpu")
-DRIVER_PATH = sa_settings.chromedriver_path
-THYNKBROWSER: Optional[WebDriver] = None
-wait = WebDriverWait(THYNKBROWSER, 1)
+def load_driver():
+    options = Options()
+    options.log.level = "trace"
+    options.add_argument("-remote-debugging-port=9224")
+    options.add_argument("-headless")
+    options.add_argument("-disable-gpu")
+    options.add_argument("-no-sandbox")
+    binary = webdriver.Firefox.firefox_binary(os.environ.get('FIREFOX_BIN'))
+    driver = webdriver.Firefox(firefox_binary=binary, executable_path=os.environ.get('GECKODRIVER_PATH'), options=options)
+    return driver
+
+BROWSER: Optional[WebDriver] = None
+wait = WebDriverWait(BROWSER, 1)
+
+async def launch_browser():
+    global BROWSER
+    BROWSER = await asyncio.to_thread(load_driver)
+
 
 
 async def launch_browser():
-    global THYNKBROWSER
+    global BROWSER
     service = Service(executable_path=DRIVER_PATH)
-    THYNKBROWSER = webdriver.Chrome(service=service, options=BROWSER_OPTIONS)
+    BROWSER = webdriver.Firefox(service=service, options=BROWSER_OPTIONS)
 
 
 async def new_tab():
-    global THYNKBROWSER
-    if THYNKBROWSER:
-        await asyncio.to_thread(THYNKBROWSER.execute_script, "window.open('about:blank', '_blank');")
-        await asyncio.to_thread(THYNKBROWSER.switch_to.window, THYNKBROWSER.window_handles[-1])
+    global BROWSER
+    if BROWSER:
+        await asyncio.to_thread(BROWSER.execute_script, "window.open('about:blank', '_blank');")
+        await asyncio.to_thread(BROWSER.switch_to.window, BROWSER.window_handles[-1])
 
 
 async def close_browser():
-    global THYNKBROWSER
-    if THYNKBROWSER:
-        await asyncio.to_thread(THYNKBROWSER.quit)
-        THYNKBROWSER = None
+    global BROWSER
+    if BROWSER:
+        await asyncio.to_thread(BROWSER.quit)
+        BROWSER = None
 
 
 async def close_tab():
-    global THYNKBROWSER
-    if THYNKBROWSER:
-        await asyncio.to_thread(THYNKBROWSER.execute_script, "window.close();")
-        await asyncio.to_thread(THYNKBROWSER.switch_to.window, THYNKBROWSER.window_handles[-1])
+    global BROWSER
+    if BROWSER:
+        await asyncio.to_thread(BROWSER.execute_script, "window.close();")
+        await asyncio.to_thread(BROWSER.switch_to.window, BROWSER.window_handles[-1])
 
 
 async def go_to_url(url: str):
-    global THYNKBROWSER
-    if THYNKBROWSER:
-        await asyncio.to_thread(THYNKBROWSER.get, url)
+    global BROWSER
+    if BROWSER:
+        await asyncio.to_thread(BROWSER.get, url)
 
 
 async def login_to_selleramp():
     try:
-        if not THYNKBROWSER:
-            await launch_browser()
+        # Create a new tab
         await new_tab()
-        if THYNKBROWSER.current_url != "https://sas.selleramp.com/site/login":
+        # Check if already on the login page
+        if BROWSER.current_url != "https://sas.selleramp.com/site/login":
+            # Navigate to the login page
             await go_to_url("https://sas.selleramp.com/site/login")
-        email = THYNKBROWSER.find_element(By.XPATH, "//*[@id='loginform-email']")
+        # Find the email field and enter the email
+        email = BROWSER.find_element(By.XPATH, "//*[@id='loginform-email']")
         email.send_keys(sa_settings.sas_email)
-        password = THYNKBROWSER.find_element(By.XPATH, "//*[@id='loginform-password']")
+        # Find the password field and enter the password
+        password = BROWSER.find_element(By.XPATH, "//*[@id='loginform-password']")
         password.send_keys(sa_settings.sas_pass)
-        login = THYNKBROWSER.find_element(By.XPATH, "//*[@id='login-form']/div[5]/button")
+        # Click the login button
+        login = BROWSER.find_element(By.XPATH, "//*[@id='login-form']/div[5]/button")
         login.click()
     except Exception as e:
         print(f"Error logging in to SellerAMP: {e}")
@@ -74,28 +88,32 @@ async def login_to_selleramp():
 
 async def search_ean_on_selleramp(ean: str):
     try:
-        if "https://sas.selleramp.com/" not in THYNKBROWSER.current_url:
+        # Check if the current URL matches the selleramp search URL
+        if "https://sas.selleramp.com/" not in BROWSER.current_url:
+            # Navigate to the login page
             url = "https://sas.selleramp.com/"
             await go_to_url(url)
-        search_field = THYNKBROWSER.find_element(By.XPATH, "//*[@id='saslookup-search_term']")
+        # Find the search field and enter the EAN
+        search_field = BROWSER.find_element(By.XPATH, "//*[@id='saslookup-search_term']")
         search_field.clear()
         search_field.send_keys(ean)
+        # Click the search button
         try:
-            search_button = THYNKBROWSER.find_element(By.XPATH,
+            search_button = BROWSER.find_element(By.XPATH,
                                                  "/html/body/div[2]/div/div/div[2]/form/div/div/div/div/div/div[2]/button")
         except NoSuchElementException:
             try:
-                search_button = THYNKBROWSER.find_element(By.XPATH,
+                search_button = BROWSER.find_element(By.XPATH,
                                                  "/html/body/div[2]/div/div/form/div[2]/div/div/div/div/div/div/div/div[2]/button")
             except NoSuchElementException:
-                search_button = THYNKBROWSER.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/form/h1/div/div/div[1]/div[2]/button[1]")
+                search_button = BROWSER.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/form/h1/div/div/div[1]/div[2]/button[1]")
         search_button.click()
 
         # Check if product loaded
-        if "https://sas.selleramp.com/sas/lookup/" not in THYNKBROWSER.current_url:
+        if "https://sas.selleramp.com/sas/lookup/" not in BROWSER.current_url:
             try:
                 # Wait for the search results to load
-                search_results = THYNKBROWSER.find_elements(By.XPATH, "//div[@id='search-results']/ul/li")
+                search_results = BROWSER.find_elements(By.XPATH, "//div[@id='search-results']/ul/li")
                 print(search_results)
                 if len(search_results) > 0:
                     # Click on the first product in the search results
@@ -111,23 +129,23 @@ async def search_ean_on_selleramp(ean: str):
 async def scrape_product_data(cost_input: float):
     try:
         scraped_data = []
-        ean = THYNKBROWSER.find_element(By.XPATH, "//*[@id='pdb-ean-input']").text
-        price = THYNKBROWSER.find_element(By.XPATH, "//*[@id='qi_sale_price']").text
+        ean = BROWSER.find_element(By.XPATH, "//*[@id='pdb-ean-input']").text
+        price = BROWSER.find_element(By.XPATH, "//*[@id='qi_sale_price']").text
         # Enter the price in the cost field
-        cost = THYNKBROWSER.find_element(By.XPATH, "//*[@id='qi_cost']")
+        cost = BROWSER.find_element(By.XPATH, "//*[@id='qi_cost']")
         cost.clear()
         cost.send_keys(cost_input)
-        product = THYNKBROWSER.find_element(By.XPATH, "//div[@class='pdb-title product-title']")
-        asin = THYNKBROWSER.find_element(By.XPATH, "//*[@id='pdb-asin-input']")
-        alerts = THYNKBROWSER.find_element(By.XPATH, "//*[@id='qi-alerts']/ul")
-        bsr = THYNKBROWSER.find_element(By.XPATH, "//*[@id='qi-bsr']")
-        est = THYNKBROWSER.find_element(By.XPATH, "//*[@id='qi-estimated-sales']/span")
-        profit = float(THYNKBROWSER.find_element(By.XPATH, "//*[@id='qi-profit']")
+        product = BROWSER.find_element(By.XPATH, "//div[@class='pdb-title product-title']")
+        asin = BROWSER.find_element(By.XPATH, "//*[@id='pdb-asin-input']")
+        alerts = BROWSER.find_element(By.XPATH, "//*[@id='qi-alerts']/ul")
+        bsr = BROWSER.find_element(By.XPATH, "//*[@id='qi-bsr']")
+        est = BROWSER.find_element(By.XPATH, "//*[@id='qi-estimated-sales']/span")
+        profit = float(BROWSER.find_element(By.XPATH, "//*[@id='qi-profit']")
                        .text.replace('£', ''))
-        roi = THYNKBROWSER.find_element(By.XPATH, "//*[@id='qi-roi']")
-        reviewCount = THYNKBROWSER.find_element(By.XPATH, "//*[@id='product-details-box']/div[3]/div[2]/span[2]")
+        roi = BROWSER.find_element(By.XPATH, "//*[@id='qi-roi']")
+        reviewCount = BROWSER.find_element(By.XPATH, "//*[@id='product-details-box']/div[3]/div[2]/span[2]")
         # Find the rating element and its children (the stars)
-        rating_element = THYNKBROWSER.find_element(By.XPATH, "//*[@id='product-details-box']/div[3]/div[2]/span[1]")
+        rating_element = BROWSER.find_element(By.XPATH, "//*[@id='product-details-box']/div[3]/div[2]/span[1]")
         stars = rating_element.find_elements(By.TAG_NAME, "i")
         # Initialize a variable to store the rating
         reviews = 0
@@ -140,9 +158,9 @@ async def scrape_product_data(cost_input: float):
                 reviews += 0
             else:
                 reviews += 1
-        fbaSellers = THYNKBROWSER.find_element(By.XPATH, "//*[@id='keepa_csv_type_10']")
-        fbmSellers = THYNKBROWSER.find_element(By.XPATH, "//*[@id='keepa_csv_type_7']")
-        amazon = THYNKBROWSER.find_element(By.XPATH, "//*[@id='keepa_csv_type_0']")
+        fbaSellers = BROWSER.find_element(By.XPATH, "//*[@id='keepa_csv_type_10']")
+        fbmSellers = BROWSER.find_element(By.XPATH, "//*[@id='keepa_csv_type_7']")
+        amazon = BROWSER.find_element(By.XPATH, "//*[@id='keepa_csv_type_0']")
         scraped_data.append({
             'asin': asin,
             'ean': ean,
